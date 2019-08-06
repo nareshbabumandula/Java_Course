16093f4556e35ad6d7a53faf517fb932a81c541c
Mandula Naresh Babu <naresh2223@gmail.com>
2019-08-06 14:42:35 -0400

    06-Aug-2019:Naresh: Primitive Datatypes

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,33 @@
+package basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		
+		// Primitive data types
+		int a =32728;
+		float b= 12.6f;
+		char c= 'f';
+		double d=23.34235;
+		long e =322352656;
+		boolean f = true;
+		boolean g = false;
+		byte h = 127;		
+		short i = 31100;
+		
+		System.out.println("Integer value of a is : " + a);
+		System.out.println("Float value of b is : " + b);
+		System.out.println("Char value of c is : " + c);
+		System.out.println("Double value of d is : " + d);
+		System.out.println("Long value of e is : " + e);
+		System.out.println("Boolean value of f is : " + f);
+		System.out.println("Boolean value of g is : " + g);
+		System.out.println("Short value of i is : " + i);
+		System.out.println("Byte value of h is : " + h);
+
+
+
+	}
+
+}
---
+++ src/basics/Shortcuts.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Shortcuts 
+{
+
+	// Run the java byte code - Ctrl+F11 or Click on Run button or Right Click on code editor>Run As>Java Application
+	// Comment a line of code use double slash //
+	// Comment or uncomment a block of code Ctrl+Shift+/
+	
+	
+
+}
